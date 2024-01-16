7f44591ec5c7e1a57cfcd77578f808742b2bf5cc
Hafiz Hamdard <ahmadshakhi2020@gmail.com>
2024-01-16 10:41:00 -0800

    sugar exercise

---
+++ sugarexample/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace sugarexample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+            string response;
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+                Console.WriteLine(response);
+            }
+            else
+            {
+                response = answer + " is greater than or equal to nine";
+                Console.WriteLine(response);
+
+            }
+
+
+            string assign= (answer <9) ? "is less than nine" : "is greater than or equal to nine";
+            Console.WriteLine(assign);
+        }
+    }
+}
